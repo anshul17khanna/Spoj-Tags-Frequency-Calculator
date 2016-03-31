521f8fbcb78a73274c7e888d3fe911c149844ed3
Anshul Khanna <anshul17khanna@gmail.com>
2016-04-01 03:32:43 +0530

    added Username chacker and fetched list of his solved problems

--- spoj tags calci.py
+++ spoj tags calci.py
@@ -11,7 +11,7 @@ def is_connected():
         pass
     return False
 if is_connected() is False:
-    print "\nNo Internet Connection!\n"
+    print "\nInternet Connection Required!\n"
     sys.exit(1)
 
 try:
@@ -20,14 +20,54 @@ except ImportError:
     print "\nmechanize not installed, please install using :\npip install mechanize\n"
     sys.exit(1)
 
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    print "\nbs4 not installed, please install using :\npip install beautifulsoup4\n"
+
 br = Browser()
 
 br.addheaders = [('connection', 'keep-alive'), ('Host', 'stackoverflow.com'),
-            ('Referer', 'https://www.google.co.in/'), ('Upgrade-Insecure-Requests', '1'),
-            ('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36')
+    ('Referer', 'https://www.google.co.in/'), ('Upgrade-Insecure-Requests', '1'),
+    ('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36')
 ]
 br.set_handle_robots(False)
 
-br.open("http://www.google.com/")
+print "\nEnter Username : ",
+username = raw_input()
+url = "http://www.spoj.com/users/" + username
+response = br.open(url)
+verify = response.read()
 
-# br.select_form
+if verify.find('@'+username) == -1:
+    print "\nUsername does not exist!\n"
+    sys.exit(1)
+
+solved = []
+
+soup = BeautifulSoup(verify, 'html.parser')
+for table in soup.find_all('table', attrs={'class': 'table-condensed'}):
+    for row in table.find_all('tr'):
+        for item in row.find_all('td'):
+            if item.get_text() != '':
+                solved.append(item.get_text())
+
+for i in range(len(solved)):
+    url = "http://www.spoj.com/problems/"+solved[i]
+    print url+'\n'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
